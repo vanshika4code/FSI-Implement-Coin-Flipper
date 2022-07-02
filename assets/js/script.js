be8f409e3f29f10d13dc7c56a9bc0c6284d62845
Vanshika <vanshika@Vanshikas-MacBook-Pro.local>
2022-07-01 23:06:16 -0700

    Penny

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,23 @@
 // TODO: Declare any global variables we need
-
+let headsFlips = 0
+let tailsFlips = 0
 
 document.addEventListener('DOMContentLoaded', function () {
+
+    document.querySelector('#flip').addEventListener('click', () => {
+      let flippedHeads = Math.random() <.5;
+       if (flippedHeads) {
+           document.querySelector('#penny-image').src ='./assets/images/penny-heads.jpg';
+           document.querySelector('#message').textContent = "You Flipped Heads!";
+           headsFlips++;
+        } else {
+        document.querySelector('#penny-image').src ='./assets/images/penny-tails.jpg';
+        document.querySelector('#message').textContent = "You Flipped Tails!";
+        tailsFlips++;
+
+
+
+    }
     // This is just a sanity check to make sure your JavaScript script is getting loaded
     // You can remove it once you see it in your browser console in the developer tools
     console.log('Hi')
@@ -13,6 +29,19 @@ document.addEventListener('DOMContentLoaded', function () {
         // TODO: Update image and status message in the DOM
 
         // Update the scorboard
+      let totalFlips = headsFlips + tailsFlips;
+      let percentHeads = 0;
+      let percentTails = 0;
+
+      if (totalFlips > 0) {
+         percentHeads = Math.round((headsFlips / totalFlips) * 100);
+         percentTails = Math.round((tailsFlips / totalFlips) * 100);
+      }
+      
+      document.querySelector("#heads").textContent = headsFlips;
+      document.querySelector("#heads-percent").textContent = percentHeads + '%';
+      document.querySelector("#tails").textContent = tailsFlips;
+      document.querySelector("#tails-percent").textContent = percentTails + '%';
         // TODO: Calculate the total number of rolls/flips
         // Make variables to track the percentages of heads and tails
         // TODO: Use the calculated total to calculate the percentages
@@ -20,8 +49,33 @@ document.addEventListener('DOMContentLoaded', function () {
         // TODO: Update the display of each table cell
 
 
+      document.querySelector("#clear").addEventListener("click", function () {
+        headsFlips = 0;
+        tailsFlips = 0;
+
+        document.querySelector('#message').textContent = "Let's Get Rolling";
+
+        let totalFlips = headsFlips + tailsFlips;
+      let percentHeads = 0;
+      let percentTails = 0;
+
+
+        if (totalFlips > 0) {
+            percentHeads = Math.round((headsFlips / totalFlips) * 100);
+            percentTails = Math.round((tailsFlips / totalFlips) * 100);
+         }
+         
+         document.querySelector("#heads").textContent = headsFlips;
+         document.querySelector("#heads-percent").textContent = percentFlips + '%';
+         document.querySelector("#tails").textContent = tailsFlips;
+         document.querySelector("#tails-percent").textContent = percentTails + '%';
+
+
+
+      }) 
+
     // Clear Button Click Handler
         // TODO: Reset global variables to 0
         // TODO: Update the scoreboard (same logic as in flip button click handler)
 
-})
+    })})
